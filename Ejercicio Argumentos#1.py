fab5f64c9b712bea00d154f2802305ed6b17aa2e
JaimeHernandez18 <jhernandez@lsv-tech.com>
2019-03-27 08:13:25 -0500

    Mejora Ejercicio Argumentos#1

--- Ejercicio Argumentos#1.py
+++ Ejercicio Argumentos#1.py
@@ -16,75 +16,98 @@ def listarContactos(*args):
         print("Nombre: {}".format(i[1]))
         print("Telefonos: {}".format(i[2]))
         print("Direccion: {direccion}".format(**i[3]))
-        print("Whatsapp: {whatsapp}".format(**i[3]))
-        print("Telegram: {telegram}".format(**i[3]))
+        print("Whatsapp: {whats}".format(**i[3]))
+        print("Telegram: {teleg}".format(**i[3]))
     print("\n-------------------------------------------------------------------------")
 
 
-def confirmar_datos(name,phone):
-    if name in agenda or phone in agenda:
-        return False
-    else:
-        return True
-
-
-vec_phone = []
-
 ids = [1]
+vec_phone = []
+vec_extracell = []
 
 
 def ingresar_datos():
 
-    whatsapp = False
-    telegram = False
-    name = input("Ingrese su nombre: ").lower()
-    phone = input("Ingrese su telefono: ").lower()
-    id = ids[len(ids)-1]
-    ids.append(id+1)
-    idcontacto = id
+    estado = True
     extracell = []
+    id = ids[len(ids) - 1]
+    ids.append(id + 1)
+    idcontacto = id
+    name = input("Ingrese su nombre: ")
+
+    while estado:
+        phone = input("Ingrese su telefono: ")
+        if len(vec_phone) != 0 and phone in vec_phone:
+            print("\nEl telefono {} ya ha sido registrado. Ingrese otro".format(phone))
+        else:
+            estado = False
+            vec_phone.append(phone)
+
     n = int(input("ingrese el numero de telefonos extras que tiene: "))
     if n != 0:
         for i in range(n):
-            cel = input("Ingrese el numero de telefono #{}: ".format(i+1))
-            extracell.append(cel)
-    vec_phone.append(phone)
+            estado= True
+            while estado:
+                cel = input("Ingrese el numero de telefono #{}: ".format(i + 1))
+                if len(vec_extracell) != 0 and cel in vec_extracell or cel in vec_phone or cel in extracell:
+                    print("\nEl telefono {} ya ha sido registrado. Ingrese otro".format(cel))
+                else:
+                    estado = False
+                    extracell.append(cel)
+
     direccion = input("Ingrese su direccion: ")
-    whats = input("Si tiene whatsapp ingrese 1 sino presiono otro numero: ")
+    whats = input("Si tiene whatsapp ingrese 1 sino presione 0: ")
     if whats == '1':
         whatsapp = True
-    else:
+    elif whats == '0':
         whatsapp = False
-    tele = input("Si tiene telegram ingrese 1 sino presiono otro numero: ")
+    tele = input("Si tiene telegram ingrese 1 sino presione 0: ")
     if tele == '1':
         telegram = True
-    else:
+    elif tele == '0':
         telegram = False
 
     return name, phone, idcontacto, extracell, direccion, whatsapp, telegram
 
+estado = True
 
-while True:
-    phone = []
-    datos = ingresar_datos()
-    name = datos[0]
-    phone.append(datos[1])
-    idcontacto = datos[2]
-    extracell = datos[3]
-    phone.extend(extracell)
-    direc = datos[4]
-    whats = datos[5]
-    teleg = datos[6]
-    if confirmar_datos(name,phone):
-        guardarContacto(idcontacto,name,phone,direccion=direc,whatsapp=whats,telegram=teleg)
-        x = input("Para seguir ingresando presione cualquier numero, sino presione 1: ")
-        if x == '1':
-            break
+while estado:
+    opt = input("""1) Ingresar contactos
+2) Mostrar contactos
+3) Salir
+""")
+    if opt < '0' or opt > '3' or not opt.isdigit():
+        print("\nIngrese una opcion valida\n")
+        continue
+    elif opt == '1':
+        estado2 = True
+        while estado2:
+            estado3 = True
+            phones = []
+            name, phone, idcontacto, extracell, direccion, whats, teleg = ingresar_datos()
+            phones.append(phone)
+            phones.extend(extracell)
+            guardarContacto(idcontacto, name, phones, direccion=direccion,whats=whats,teleg=teleg)
+            while estado3:
+                opt = input("""\n1) Para ingresar otro contacto
+2) Para dejar de ingresar contactos
+""")
+                if opt < '0' or opt > '2' or not opt.isdigit():
+                    print("\nIngrese una opcion valida\n")
+                    continue
+                elif opt == '1':
+                    estado3 = False
+
+                elif opt == '2':
+                    estado3 = False
+                    estado2 = False
+    elif opt == '2':
+        if len(agenda) == 0:
+            print("\nNo hay registros\n")
         else:
-            continue
-    else:
-        print("Los datos recien ingresados ya estan en el diccionario, ingrese otros")
+            listarContactos(agenda)
+            print("")
+    elif opt == '3':
+        break
 
 print("\n")
-
-listarContactos(agenda)
