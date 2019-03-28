2f8716e2e73ff8f329dc80cebcc31dedd635a4fd
JaimeHernandez18 <jhernandez@lsv-tech.com>
2019-03-28 17:27:59 -0500

    Añadir ejercicio Server

---
+++ Ejercicio Server/Server.py
@@ -0,0 +1,11 @@
+import http.server
+
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Servidor en el puerto 8000")
+    httpd.serve_forever()
